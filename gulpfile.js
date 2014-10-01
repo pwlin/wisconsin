df0359ff29ad40b01019baea52f8c959de1f451b
pwlin <pwlin05@gmail.com>
2014-10-01 19:49:54 +0200

    Modified the gulp file to generate versioned css and js

--- gulpfile.js
+++ gulpfile.js
@@ -26,21 +26,8 @@ var paths = {
     ],
 
     styles: [
-        './node_modules/fries/dist/themes/holo-dark/holo-dark.min.css',
-        /*'./node_modules/fries/dist/css/holo-dark/action-bars.css',
-        './node_modules/fries/dist/css/holo-dark/animations.css',
-        './node_modules/fries/dist/css/holo-dark/base.css',
-        './node_modules/fries/dist/css/holo-dark/buttons.css',
-        './node_modules/fries/dist/css/holo-dark/content.css',
-        './node_modules/fries/dist/css/holo-dark/dialogs.css',
-        './node_modules/fries/dist/css/holo-dark/forms.css',
-        './node_modules/fries/dist/css/holo-dark/icomoon.css',
-        './node_modules/fries/dist/css/holo-dark/lists.css',
-        './node_modules/fries/dist/css/holo-dark/sliders.css',
-        './node_modules/fries/dist/css/holo-dark/spinners.css',
-        './node_modules/fries/dist/css/holo-dark/stack.css',
-        './node_modules/fries/dist/css/holo-dark/tabs.css',
-        './node_modules/fries/dist/css/holo-dark/toasts.css',*/
+        //'./node_modules/fries/dist/themes/holo-dark/holo-dark.min.css',
+        './node_modules/fries/dist/css/holo-dark/*.css',
         './src/styles/basic.less'
     ],
 
@@ -58,36 +45,31 @@ var paths = {
 
 };
 
-gulp.task('scripts', [], function () {
-    del(['./www/files/scripts'], function () {
+gulp.task('all', [], function () {
+    var tsJS = require('crypto').createHash('md5').update('js-' + new Date().valueOf()).digest('hex'),
+        tsCSS = require('crypto').createHash('md5').update('css-' + new Date().valueOf()).digest('hex');
+
+    del(['./www/files/scripts',
+         './www/files/styles/halo-dark',
+         './public/files/fonts',
+         './www/*.html'
+        ], function () {
         gulp.src(paths.scripts)
-            .pipe(concat('js.js'))
+            .pipe(concat(tsJS + '.js'))
             .pipe(uglify())
             .pipe(gulp.dest('./www/files/scripts'));
-    });
-});
 
-gulp.task('styles', [], function () {
-    del(['./www/files/styles/halo-dark'], function () {
         gulp.src(paths.styles)
-            .pipe(concat('css.css'))
+            .pipe(concat(tsCSS + '.css'))
             .pipe(less())
             .pipe(minifyCSS({
                 keepSpecialComments: 0
             }))
             .pipe(gulp.dest('./www/files/styles/halo-dark'));
-    });
-});
 
-gulp.task('fonts', [], function () {
-    del(['./public/files/fonts'], function () {
         gulp.src(paths.fonts)
             .pipe(gulp.dest('./www/files/fonts'));
-    });
-});
 
-gulp.task('html', [], function () {
-    del(['./www/*.html'], function () {
         gulp.src(paths.html)
             .pipe(fileInclude())
             .pipe(minifyHTML({
@@ -95,16 +77,21 @@ gulp.task('html', [], function () {
                 spare: true,
                 quotes: true
             }))
+            .pipe(replace('$$JS_OUT$$', tsJS))
+            .pipe(replace('$$CSS_OUT$$', tsCSS))
             .pipe(replace(/\r\n|\n/g, ''))
+            .pipe(replace(/\s{2,}/g, ' '))
             .pipe(gulp.dest('./www'));
     });
+
 });
+
 
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles, ['styles']);
-    gulp.watch(paths.fonts, ['fonts']);
-    gulp.watch((paths.html).concat(paths.htmlPartials), ['html']);
+    gulp.watch(paths.scripts, ['all']);
+    gulp.watch(paths.styles, ['all']);
+    gulp.watch(paths.fonts, ['all']);
+    gulp.watch((paths.html).concat(paths.htmlPartials), ['all']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'fonts', 'html']);
+gulp.task('default', ['all']);
